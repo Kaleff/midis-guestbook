298bc43d7c7195261043030279b73e3a8fdf22be
Marks Kalijevs <markskalijevs@gmail.com>
2025-05-12 07:37:16 +0300

    [MAIN] Add action icons

--- resources/js/pages/Welcome.vue
+++ resources/js/pages/Welcome.vue
@@ -172,6 +172,7 @@ const closeModal = () => {
                                     <th class="pb-2 text-left font-medium">Name</th>
                                     <th class="pb-2 text-left font-medium">Message</th>
                                     <th class="pb-2 text-left font-medium">Posted</th>
+                                    <th class="pb-2 text-left font-medium">Actions</th>
                                 </tr>
                             </thead>
                             <tbody>
@@ -179,6 +180,40 @@ const closeModal = () => {
                                     <td class="py-3 pr-4">{{ post?.name || 'Anonymous' }}</td>
                                     <td class="py-3 pr-4">{{ post?.text || 'No message content' }}</td>
                                     <td class="py-3 text-[#706f6c] dark:text-[#A1A09A]">{{ post?.created_at }}</td>
+                                    <td class="py-3">
+                                        <div class="flex space-x-2">
+                                            <!-- View Icon -->
+                                            <button
+                                                class="rounded-full p-1.5 hover:bg-gray-100 dark:hover:bg-gray-800 text-gray-600 dark:text-gray-300"
+                                                title="View"
+                                            >
+                                                <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
+                                                </svg>
+                                            </button>
+
+                                            <!-- Edit Icon -->
+                                            <button
+                                                class="rounded-full p-1.5 hover:bg-gray-100 dark:hover:bg-gray-800 text-blue-600 dark:text-blue-400"
+                                                title="Edit"
+                                            >
+                                                <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
+                                                </svg>
+                                            </button>
+
+                                            <!-- Delete Icon -->
+                                            <button
+                                                class="rounded-full p-1.5 hover:bg-gray-100 dark:hover:bg-gray-800 text-red-600 dark:text-red-400"
+                                                title="Delete"
+                                            >
+                                                <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                                                </svg>
+                                            </button>
+                                        </div>
+                                    </td>
                                 </tr>
                             </tbody>
                         </table>
